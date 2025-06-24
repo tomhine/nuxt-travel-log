f99e06ce1915efce56b507033c27b2cb295e8d43
Tom Hine <tomhine792@gmail.com>
2025-06-24 22:49:54 +0100

    setup tailwind | daisy ui | icons

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-[80dvh] grid place-items-center">
+    <button class="btn btn-secondary gap-2">
+      Open Map
+      <Icon name="tabler:map" size="1.75em" />
+    </button>
   </div>
 </template>
