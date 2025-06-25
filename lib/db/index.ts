9c8eccc1acbfbbea32b1e5fd99f30f0b810605cc
Tom Hine <tomhine792@gmail.com>
2025-06-26 00:23:10 +0100

    setuo db (#53)
    * set db
    
    * add env variables to workflow

---
+++ lib/db/index.ts
@@ -0,0 +1,17 @@
+import { drizzle } from "drizzle-orm/libsql";
+
+import env from "~/lib/env";
+
+import * as schema from "./schema";
+
+// You can specify any property from the libsql connection options
+const db = drizzle({
+  connection: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
+  },
+  casing: "snake_case",
+  schema,
+});
+
+export default db;
