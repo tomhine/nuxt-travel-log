d6625937045b2dcee98c2ce732b26ae477610c50
Tom Hine <tomhine792@gmail.com>
2025-06-26 23:35:11 +0100

    setup github auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+  ],
   eslint: {
     config: {
       standalone: false,
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container mx-auto max-w-5xl grid place-items-center p-8">
+    <h1 class="text-3xl font-semibold">
+      You are logged in! 🎉
+    </h1>
+  </div>
+</template>
