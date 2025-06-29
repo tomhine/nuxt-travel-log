48986b0a0c1f193b41e3b66e376951614dcfe593
Tom Hine <tomhine792@gmail.com>
2025-06-29 14:40:56 +0100

    auth protect dashboard routes & add titles (#57)

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+const mainTitle = "Travel Log";
+useHead({
+  titleTemplate: (page) => {
+    return page
+      ? `${page} - ${mainTitle}`
+      : mainTitle;
+  },
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: "Home",
+});
+
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +16,14 @@
         <p class="py-6">
           Keep track of your travels and adventures. Add locations, photos and notes to create a digital journal of your journeys.
         </p>
-        <AuthButton />
+        <NuxtLink v-if="authStore.user" to="/dashboard" class="btn btn-primary">
+          <Icon
+            name="tabler:arrow-right"
+            size="24"
+          />
+          Get started
+        </NuxtLink>
+        <AuthButton v-else />
       </div>
     </div>
   </div>
