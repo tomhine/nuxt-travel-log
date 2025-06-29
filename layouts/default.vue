df1531583b54bd9907da9c188ce73febce11edac
Tom Hine <tomhine792@gmail.com>
2025-06-29 15:16:41 +0100

    ssr auth session setup (#58)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+await authStore.init();
+</script>
+
 <template>
   <div class="flex min-h-[100dvh] flex-col">
     <AppNavBar />
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -6,7 +6,7 @@ export default defineEventHandler(async (event) => {
       headers: event.headers,
     });
 
-    if (!session) {
+    if (!session?.user) {
       await sendRedirect(event, "/", 302);
     }
   }
